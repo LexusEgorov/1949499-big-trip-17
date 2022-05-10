8a55321a84bcbf6419a043d7b7eb3d28aea39b0f
lexusegorov <alexey.egorov.job@gmail.com>
2022-05-10 18:42:39 +0700

    Добавил работу с моделью

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import {
-  render
-} from './render.js';
-import FilterView from './view/filter-view.js';
 import ListPresenter from './presenter/list-presenter.js';
+import HeaderPresenter from './presenter/header-presenter.js';
+import PointModel from './model/point-model.js';
 
 const siteFilter = document.querySelector('.trip-controls__filters');
 const siteEvents = document.querySelector('.trip-events');
-const listPresenter = new ListPresenter();
 
-render(new FilterView(), siteFilter);
-listPresenter.init(siteEvents);
+const listPresenter = new ListPresenter();
+const headerPresenter = new HeaderPresenter();
+
+const pointsModel = new PointModel().getPoints();
+
+headerPresenter.init(siteFilter);
+listPresenter.init(siteEvents, pointsModel);
