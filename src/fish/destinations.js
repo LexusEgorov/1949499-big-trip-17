22844a3b06a1b6d29f612da478f9c8d3bd368c0b
lexusegorov <alexey.egorov.job@gmail.com>
2022-05-10 18:40:18 +0700

    Генератор рыбных данных

---
+++ src/fish/destinations.js
@@ -0,0 +1,36 @@
+import { getRandomInteger } from '../util.js';
+
+const MAX_RANDOM_PHOTO = 1000;
+const MAX_COUNT_PHOTOS = 5;
+
+const getPictures = () => {
+  const photos = Array.from({
+    length: getRandomInteger(1, MAX_COUNT_PHOTOS)
+  }, () => ({
+    src: `http://picsum.photos/248/152?r=${getRandomInteger(0, MAX_RANDOM_PHOTO)}`,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  }));
+
+  return photos;
+};
+
+const destinations = [
+  {
+    name: 'Novosibirsk',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+    pictures: getPictures(),
+  },
+  {
+    name: 'Abakan',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.',
+    pictures: getPictures(),
+  },
+];
+
+const mapDestinations = new Map();
+
+destinations.forEach((element) => {
+  mapDestinations.set(element.name, element);
+});
+
+export {mapDestinations};
