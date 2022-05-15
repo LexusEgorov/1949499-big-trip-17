8720d6e7bdce623b74054faf47e4f4e5eb5d358c
lexusegorov <alexey.egorov.job@gmail.com>
2022-05-15 16:24:15 +0700

    Рефакторинг

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import ListPresenter from './presenter/list-presenter.js';
-import HeaderPresenter from './presenter/header-presenter.js';
 import PointModel from './model/point-model.js';
 
 const siteFilter = document.querySelector('.trip-controls__filters');
 const siteEvents = document.querySelector('.trip-events');
 
 const listPresenter = new ListPresenter();
-const headerPresenter = new HeaderPresenter();
 
 const pointsModel = new PointModel().points;
 
-headerPresenter.init(siteFilter);
-listPresenter.init(siteEvents, pointsModel);
+listPresenter.init(siteEvents, siteFilter, pointsModel);
